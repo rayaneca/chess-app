153b28b8834d6b2aaf1b8558ff65153c041bc811
Mohamed Elhadi <computersciencemohamed@gmail.com>
2026-07-14 19:24:40 +0200

    WE chanage the name

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-black">
       <Text className="text-xl font-bold text-red-500 text-center">
-        Welcome  to chess app by ryan and moooh
+        Welcome  to chess app by ryan and moooh chess 
       </Text>
     </View>
   );
